a9191b203f4214cedeedf411b82b54c472a9a963
varun <rvarungoud20@gmail.com>
2024-12-15 18:45:12 +0530

    changes

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -47,6 +47,7 @@ const login = asyncHandler(async (req, res) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     maxAge: 24 * 60 * 60 * 1000,
+    sameSite: "None",
   });
 
   res.status(200).json({ token });
