62606ea8512043c58f1b10f180641b38d241ce03
varun <rvarungoud20@gmail.com>
2024-12-16 11:26:36 +0530

    chnages

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,11 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const app = express();
 import dotenv from "dotenv";
@@ -28,5 +33,6 @@ app.use("/api/v1/jobs", jobRouter);
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
+console.log("path", path.resolve(__dirname));
 
 export default app;
